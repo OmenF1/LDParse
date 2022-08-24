7727a90e2c47ecdc6ee754cafe5583dc2d5cbb62
OmenF1 <owen.lingza@outlook.com>
2022-08-24 23:10:53 +0200

    removed usings from models.

--- LDParse/Classes/Channel.cs
+++ LDParse/Classes/Channel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LDParse.Classes
+﻿namespace LDParse.Classes
 {
     internal class Channel
     {
--- LDParse/Program.cs
+++ LDParse/Program.cs
@@ -1,5 +1,5 @@
 ﻿using LDParse;
-const string filePath = @"C:\Users\Owen\Documents\Dev\LD Reverse Eng\Hungaroring-mclaren_720s_gt3-0-2022.08.21-21.35.29.ld";
+const string filePath = @"file_path_here.";
 
 Parser parser = new Parser(filePath);
 parser.Parse();
